515efd373174dafb7d14d8e67638b3061414848e
Jester Cornejo <jcornejo3@nait.ca>
2019-09-06 12:33:54 -0600

    Progression 1

--- OOPs-Solution/OOPs-Review/FencePanel.cs
+++ OOPs-Solution/OOPs-Review/FencePanel.cs
@@ -12,6 +12,22 @@ namespace OOPs_Review
 
     public class FencePanel
     {
+        //properties
+        //property is associated with a single piece of data
+        //property has 2 self components:
+        // get : returns a value to the calling agent(outside user).
+        // set : recieves a value from the calling agent
+        //      the key word used to represent the incoming data
+        //      is value
+        //a property DOES NOT have in a developer's parameter 
 
+        //two ways to code a propert
+        //
+        //--Auto Implemented
+        //  a private data member DOES NOT need to be coded
+        //  the system will create an internal data that 
+        //      the system will manage
+
+        public double Height { get; set; }
     }
 }
